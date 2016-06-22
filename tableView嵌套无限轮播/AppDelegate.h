cd538ef510649dbc6ec54aee37b845a2ac8355ee
残月 <632342527@qq.com>
2016-06-22 14:31:29 +0800

    Initial Commit

---
+++ tableView嵌套无限轮播/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableView嵌套无限轮播
+//
+//  Created by 王奥东 on 16/6/22.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tableView嵌套无限轮播/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableView嵌套无限轮播
+//
+//  Created by 王奥东 on 16/6/22.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
